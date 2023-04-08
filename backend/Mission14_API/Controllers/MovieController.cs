b15aeb75797ac812986b40b36e3de7c773721304
Hale <crofth@byu.edu>
2023-04-07 21:30:45 -0600

    Created MovieData API

---
+++ backend/Mission14_API/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission14_API.Data;
+
+namespace Mission14_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class MovieController : Controller
+    {
+        private MovieDbContext context;
+
+        public MovieController(MovieDbContext movieDbContext) 
+        {
+            context = movieDbContext;
+        }
+
+        public IEnumerable<Movie> Get()
+        {
+            return context.Movies.Where(x => x.Edited == "Yes").OrderBy(x => x.Title).ToArray();
+        }
+    }
+}
